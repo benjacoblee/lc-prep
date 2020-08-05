757958e9fd1ba5d6a9c6a49a1d11b36366e39ed7
benn <benjacoblee@gmail.com>
2020-08-05 10:28:54 +0800

    basic implementation of doubly linked list

---
+++ linked-lists/doubly-linked-list.js
@@ -0,0 +1,48 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = null;
+    }
+
+    push(record) {
+        let node = new Node(record);
+        if (!this.head) {
+            // in the case where there's no head, the inserted node is both the head and the tail
+            this.head = node;
+            this.tail = node;
+        } else {
+            node.prev = this.tail; // the newest inserted node's previous element is whatever resides at the tail
+            this.tail.next = node; // updates the tail's next property and changes the value
+            this.tail = node;
+        }
+        this.length++;
+    }
+
+    pop() {
+        if (!this.head) return null;
+
+        const prevNode = this.tail.prev; // get a reference to the node just before the last node
+        if (prevNode) {
+            prevNode.next = null; // remove the last node
+            this.tail = prevNode; // update tail
+        } else {
+            this.head = null;
+            this.tail = null;
+        }
+        this.length--;
+    }
+}
+
+let doublyLinkedList = new DoublyLinkedList();
+doublyLinkedList.push(1);
+doublyLinkedList.push(2);
+console.log(doublyLinkedList);
