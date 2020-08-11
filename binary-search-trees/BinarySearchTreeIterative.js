8f84e1690b43262927d67bafec0be2acc9a4fb8e
benn <benjacoblee@gmail.com>
2020-08-11 11:38:14 +0800

    add bst iterative basic implementation

---
+++ binary-search-trees/BinarySearchTreeIterative.js
@@ -0,0 +1,84 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BST {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+        if (!this.root) {
+            this.root = newNode;
+            return this;
+        } else {
+            let current = this.root;
+            while (true) {
+                if (value === current.value) {
+                    return undefined;
+                }
+                if (value < current.value) {
+                    if (current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                } else if (value > current.value) {
+                    if (current.right === null) {
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
+
+    find(value) {
+        // let current = this.root;
+        // if (!current) return false;
+        // while (true) {
+        //     if (!current) return false;
+        //     if (value === current.value) return current;
+        //     if (value < current.value) {
+        //         current = current.left;
+        //     } else if (value > current.value) {
+        //         current = current.right;
+        //     }
+        // }
+
+        if (this.root === null) return false;
+        let current = this.root;
+        let found = false;
+        while (current && !found) {
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+        if (!found) return undefined;
+        return current;
+    }
+}
+
+const bst = new BST();
+bst.insert(10);
+bst.insert(5);
+bst.insert(2);
+bst.insert(13);
+bst.insert(11);
+bst.insert(16);
+bst.insert(7);
+bst.insert(7);
+console.log(bst.find(7));
+// console.log(bst);
